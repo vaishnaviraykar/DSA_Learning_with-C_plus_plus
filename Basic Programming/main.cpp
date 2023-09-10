dd3b918a97d8283013891b1f80efef52acdf2a9c
vaishnaviraykar <vaishnavisraykar2002@gmail.com>
2023-09-10 23:04:01 +0530

    first program in C++

--- Basic Programming/main.cpp
+++ Basic Programming/main.cpp
@@ -1,6 +1,9 @@
+// header file for input output
 #include <iostream>
 using namespace std;
-int main(){
+
+int main()
+{
     // print Welcome to DSA World!!
     cout <<"Welcome to DSA World!!";
     return 0;
